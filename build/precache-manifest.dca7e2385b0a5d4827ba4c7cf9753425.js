77edc5ea2bb229cd6d462db375a382593e7ab699
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2020-03-12 08:04:05 -0400

    added build folder files

---
+++ build/precache-manifest.dca7e2385b0a5d4827ba4c7cf9753425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebcb845d348bb0b1534dc49ff24103e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f415fbb5ab02783c625",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3feba4f974fd7d105f30",
+    "url": "/static/css/main.12a7095e.chunk.css"
+  },
+  {
+    "revision": "8f415fbb5ab02783c625",
+    "url": "/static/js/2.cf3ee9c9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cf3ee9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3feba4f974fd7d105f30",
+    "url": "/static/js/main.c5a92e6b.chunk.js"
+  },
+  {
+    "revision": "1961f14b8ef5a3f2d3c3",
+    "url": "/static/js/runtime-main.542e7d2f.js"
+  }
+]);
