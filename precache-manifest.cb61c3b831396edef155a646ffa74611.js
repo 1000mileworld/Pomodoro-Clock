f4464085434459d707f071f8180961f3e7f9fe4a
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-03-12 21:22:51 -0400

    Updates

---
+++ precache-manifest.cb61c3b831396edef155a646ffa74611.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7f323f79c79e2f1eeffa093e43c2cc",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "8f415fbb5ab02783c625",
+    "url": "/Pomodoro-Clock/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1c287408c352d1050b6e",
+    "url": "/Pomodoro-Clock/static/css/main.12a7095e.chunk.css"
+  },
+  {
+    "revision": "8f415fbb5ab02783c625",
+    "url": "/Pomodoro-Clock/static/js/2.cf3ee9c9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Pomodoro-Clock/static/js/2.cf3ee9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c287408c352d1050b6e",
+    "url": "/Pomodoro-Clock/static/js/main.4c6e0064.chunk.js"
+  },
+  {
+    "revision": "e9a5209b87860f0ede37",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.b1db48a2.js"
+  }
+]);
